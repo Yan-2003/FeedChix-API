43aa07894f49624141120a8bb57465591d2cb792
Julliane Tampus <julliane.tampus123@gmail.com>
2025-04-12 12:44:38 +0800

    fxing bug gihapon

--- router/Food.js
+++ router/Food.js
@@ -123,7 +123,7 @@ const scheudleFood = async ()=> {
     
                     console.log("Feeding Chicken deducting food weight...")
     
-                    if(food_weight_storage.food_weight != 0){
+                    if(food_weight_storage.food_weight > 0){
                         
                         let gram_feed = chicken_data.week_age > feeding_amount_per_chick.length ? feeding_amount_per_chick[feeding_amount_per_chick.length - 1] : feeding_amount_per_chick[chicken_data.week_age] 
                         
